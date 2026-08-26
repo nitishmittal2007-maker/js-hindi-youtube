881db3c1e8515b7eb0b9598080caa92412ee7aa4
nitishmittal2007-maker <nitishmittal2007@gmail.com>
2026-08-26 12:08:49 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "Nitish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "Nitish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// 33 => 33 number to string conversion
