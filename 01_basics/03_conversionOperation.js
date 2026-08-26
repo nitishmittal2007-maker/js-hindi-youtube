e4b2553631f11f59112510b7b43fca4ce0a67ad7
nitishmittal2007-maker <nitishmittal2007@gmail.com>
2026-08-26 14:49:30 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,7 +24,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
 // 33 => 33 number to string conversion
+
+// ****************** OPERATIONS ********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)// output is 0.666667
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " nitish"
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++; // ++a and a++ are different things.
+console.log(gameCounter);
