197b8fd89d3f659668027e19b1154776b29fa4d1
nitishmittal2007-maker <nitishmittal2007@gmail.com>
2026-08-26 15:06:57 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1); // output in true/false
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+//  the reason is that an equality check (==) and comparisons (>,<,>=,<=) work differently, comparisons convert null to a number, treating it as zero.
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// we generally avoid these null and undefined comparisons.
+
+// ===
+
+console.log("2" === 2); // (===)check data type also before compairing.
+
