6324416f0e15cf1969c33c8d270407b3627e947a
nitishmittal2007-maker <nitishmittal2007@gmail.com>
2026-08-27 12:06:19 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "nitish"
+const repoCount = 3
+
+// console.log(name + repoCount + " Value") do not use this type of syntax
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gamename = new String('jffkkn-njj')
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(6));
+// console.log(gamename.indexOf('n'));
+
+// const newString = gamename.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gamename.slice(-9, 4)
+// console.log(anotherString);
+
+const newStringOne = "    NITISH    "
+console.log(newStringOne.trim());// see documentation on trim javascript mdn.
+
+const url = "https://nitish.com%20nitishmittal"
+console.log(url.replace('%20', '-'))
+console.log(url.includes('sundar'))
+
+const gamename1 = new String('nitish-hc-com')
+console.log(gamename1.split('-'));
