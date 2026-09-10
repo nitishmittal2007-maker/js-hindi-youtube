b3f63d42089b8abf18ef673cb802be73569ee958
nitishmittal2007-maker <nitishmittal2007@gmail.com>
2026-09-10 19:27:35 +0000

    dates

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,23 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026, 8, 14)
+// let myCreatedDate = new Date(2026, 8, 14, 15, 55, 43)
+// let myCreatedDate = new Date("2026-05-23")
+let myCreatedDate = new Date("05-23-2026")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);// give value in milliseconds
+console.log(myCreatedDate.getTime());// give value in milliseconds
+console.log(Math.floor(Date.now()/1000));// give value in seconds
